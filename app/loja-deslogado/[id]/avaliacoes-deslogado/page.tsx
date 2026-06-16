209ab299b7341bf77024d831070c62a047fe244a
eduardabiason <dudabiasonalmeida@gmail.com>
2026-06-16 15:17:43 -0300

    feat: ajustes em imports

--- app/loja-deslogado/[id]/avaliacoes-deslogado/page.tsx
+++ app/loja-deslogado/[id]/avaliacoes-deslogado/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import Sidebar from "@/coelho-branco-front/components/marcuscomp/Sidebar";
 
 interface Avaliacao {
   id: number;
